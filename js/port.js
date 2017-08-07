b83b51a0dbe0c5e00db4ae33fd55de771a12ed7d
Darkzerox <darknesscs2@gmail.com>
2017-08-07 18:37:01 +0700

    fixed

--- js/port.js
+++ js/port.js
@@ -1,21 +1,20 @@
-$.getJSON( "/portfolio.json", function( data ) {
-  var items = [];
-  $.each( data, function( key, field ) {
-     items.push( `
-       <div class="col-sm-4 portfolio-item">
-         <a href="#${field.name}" class="portfolio-link" data-toggle="modal" rel="noindex nofollow">
-           <div class="caption">
-             <div class="caption-content">
-               <i class="fa fa-search-plus fa-3x"></i>
-               <div class="port-pop-title"><h2>${field.name}</h2></div>
-             </div>
-           </div>
-           <img src="img/portfolio/${field.img}" class="img-responsive" alt="${field.name}">
-         </a>
-       </div>
-       ` );
-
+$.getJSON("/portfolio.json", function(data) {
+  var port_data = [];
+  $.each(data, function(key, field) {
+    port_data.push(`
+      <div class="col-sm-4 portfolio-item group-${field.cateogry}">
+        <a href="#${field.name}" class="portfolio-link" data-toggle="modal" rel="noindex nofollow">
+          <div class="caption">
+            <div class="caption-content">
+              <i class="fa fa-search-plus fa-3x"></i>
+              <div class="port-pop-title"><h2>${field.name}</h2></div>
+            </div>
+          </div>
+          <img src="img/portfolio/${field.img}" class="img-responsive" alt="${field.name}">
+        </a>
+      </div>
+    `);
   });
 
-  $(items.join( "" )).appendTo( ".portfolio-list" );
+  $(port_data.join("")).appendTo(".portfolio-list");
 });
